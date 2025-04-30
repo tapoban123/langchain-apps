febce5b650f6c08b7d6d867384a340fbaf468b20
Tapoban Ray <raytapoban@gmail.com>
2025-04-30 13:46:00 +0530

    Sentiment Analysis working fine

--- customer_sentiment_analysis_chatbot/gen_ai/custom_prompt_templates.py
+++ customer_sentiment_analysis_chatbot/gen_ai/custom_prompt_templates.py
@@ -30,13 +30,14 @@ neg_template = PromptTemplate(
     template="""Provide a short and simple message for the negative feedback provided by the user.
     Examples:
     Review: "I went to buy a specific product at this store, but they were out of stock. I'm disappointed that they don't keep their inventory up-to-date."
-
     Response: "Dear customer, I apologize for the inconvenience of us being out of stock of the product you were looking for. We do our best to keep our inventory up-to-date, but sometimes we experience unexpected demand or supply chain disruptions. I've shared your feedback with our inventory management team, and they will be looking for ways to improve our forecasting accuracy."
     
     Review: "I was shocked at how expensive the prices were at this store. I feel like I was overcharged for what I bought."
-
     Response: "Dear customer, I understand your concern. We strive to offer fair and competitive pricing, but we also recognize that everyone has different budgets. If you'd like to discuss your purchase in more detail, please feel free to contact me directly. I may be able to offer you a discount or refund."
-    \n{user_feedback}""",
+    
+    Review: {user_feedback}
+    Response: 
+    """,
     input_variables=["user_feedback"],
 )
 
