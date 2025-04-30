a5f24524bebf4eca71edfd9939ac7264dc65c928
Tapoban Ray <raytapoban@gmail.com>
2025-05-01 01:15:28 +0530

    Added some features to SentimentAnalysisBot

--- customer_sentiment_analysis_chatbot/main.py
+++ customer_sentiment_analysis_chatbot/main.py
@@ -16,25 +16,34 @@ for var in session_vars:
 st.session_state[user_feedback] = st.text_area(
     "Enter user feedback",
     placeholder="e.g: Their staff is not only friendly but also highly skilled.",
+    key="feedback",
 )
+
 
 get_response = st.button(label="Analyze")
 
 if get_response:
     if st.session_state[user_feedback] is not None:
         response = generate_content(st.session_state[user_feedback])
-        
-        if response.get("sentiment") ==  "positive":
+        st.markdown("### User Feedback:")
+        st.write(st.session_state[user_feedback])
+        st.session_state[user_feedback] = ""
+
+        st.divider()
+
+        if response.get("sentiment") == "positive":
             st.markdown(f"### Message")
-            st.markdown(response.get('feedback_response'))
-            
+            st.markdown(response.get("feedback_response"))
+
         else:
             st.markdown(f"### Message for the user:")
-            st.markdown(response.get('feedback_response'))
-            
+            st.markdown(response.get("feedback_response"))
+
+            st.divider()
+
             st.markdown(f"\n\n### Key Concerns:")
-           
-            concern_count = 1 
+
+            concern_count = 1
             for concern in response.get("key_concerns"):
                 st.markdown(f"{concern_count}. {concern}")
                 concern_count += 1
